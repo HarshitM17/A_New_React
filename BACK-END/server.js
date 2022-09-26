5bc9a51bf9fe6d453aa44a259db8ad5d402eff9f
HarshitM17 <mathurharshit88@gmail.com>
2022-09-26 16:58:13 +0530

    new

--- BACK-END/server.js
+++ BACK-END/server.js
@@ -1,14 +1,37 @@
+require('dotenv').config();
+
 const express = require("express");
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts')
 
 //express application
 const app = express();
 
-//routes
-app.get('/',(req,res)=>{
-    res.json({mssg:"Welcome to app"})
+//func for every req
+//middleware
+app.use(express.json())
+app.use((req,res,next)=>{
+    console.log(req.path,req.method);
+    next();
 })
 
-//listen for requests
-app.listen(4000,()=>{
-    console.log("Listening on port 4000")
-})
+//routes
+// app.get('/',(req,res)=>{
+//     res.json({mssg:"Welcome to app"});
+// })
+app.use('/api/workouts',workoutRoutes);
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        //listen for requests
+    app.listen(process.env.PORT,()=>{
+        console.log("connecting database and listening on port 4000",process.env.PORT);
+        })
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+
+
+// process.env
